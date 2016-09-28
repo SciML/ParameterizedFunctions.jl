d308d46b7a6a6a5e703feae57e0e465317268614
Christopher Rackauckas <Me@ChrisRackauckas.com>
2016-09-28 09:42:15 -0700

    build factorized jacobian functions

--- src/ParameterizedFunctions.jl
+++ src/ParameterizedFunctions.jl
@@ -56,16 +56,18 @@ macro ode_def(name,ex,params...)
 
   local fsymjac_L
   local fsymjac_U
+  local Jex_L
+  local Jex_U
   try
     # Factorize the Jacobian
     fsymjac = lufact(symjac)
     fsymjac_L = fsymjac[:L]
     fsymjac_U = fsymjac[:U]
+    Jex_L,Jex_U = build_fjac_func(fsymjac_L,fsymjac_U,indvar_dict,param_dict,inline_dict)
   catch
     fsymjac_L = Matrix{SymEngine.Basic}(0,0)
     fsymjac_U = Matrix{SymEngine.Basic}(0,0)
   end
-
 
   # Build the type
   f = maketype(name,param_dict,origex,funcs,syms)
@@ -140,6 +142,44 @@ function build_jac_func(symjac,indvar_dict,param_dict,inline_dict)
   Jex.head = :block
   push!(Jex.args,nothing)
   Jex = :(jac = (t,u,J)->$Jex)
+end
+
+function build_fjac_func(symjac_L,symjac_U,indvar_dict,param_dict,inline_dict)
+  # Lower Triangle
+  Jex = :()
+  for i in 1:size(symjac_L,1)
+    for j in 1:i
+      ex = parse(string(symjac_L[i,j]))
+      if typeof(ex) <: Expr
+        ode_findreplace(ex,ex,indvar_dict,param_dict,inline_dict)
+      else
+        ex = ode_symbol_findreplace(ex,indvar_dict,param_dict,inline_dict)
+      end
+      push!(Jex.args,:(J[$i,$j] = $ex))
+    end
+  end
+  Jex.head = :block
+  push!(Jex.args,nothing)
+  Jex_L = :(jac = (t,u,J)->$Jex)
+
+  # Upper Triangle
+  Jex = :()
+  for j in 1:size(symjac_U,2)
+    for i in 1:j
+      ex = parse(string(symjac_U[i,j]))
+      if typeof(ex) <: Expr
+        ode_findreplace(ex,ex,indvar_dict,param_dict,inline_dict)
+      else
+        ex = ode_symbol_findreplace(ex,indvar_dict,param_dict,inline_dict)
+      end
+      push!(Jex.args,:(J[$i,$j] = $ex))
+    end
+  end
+  Jex.head = :block
+  push!(Jex.args,nothing)
+  Jex_U = :(jac = (t,u,J)->$Jex)
+
+  Jex_L,Jex_U
 end
 
 function ode_symbol_findreplace(ex,indvar_dict,param_dict,inline_dict)
