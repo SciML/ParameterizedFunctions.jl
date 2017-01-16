d7bfb8df026b770b9c26081db590d6a1c020998e
ChrisRackauckas <me@chrisrackauckas.com>
2017-01-16 10:30:37 -0800

    don't try symbolic with bare

--- src/ode_def_opts.jl
+++ src/ode_def_opts.jl
@@ -77,149 +77,151 @@ function ode_def_opts(name::Symbol,opts::Dict{Symbol,Bool},ex::Expr,params...;M=
   param_symjac = Matrix{SymEngine.Basic}(numsyms,numparams)
   pderiv_exists = false
 
-  try #do symbolic calculations
+  if opts[:build_tgrad] || opts[:build_jac] || opts[:build_dpfuncs]
+    try #do symbolic calculations
 
-    # Declare the SymEngine symbols
-    symtup,paramtup = symbolize(syms,param_dict.keys)
-    depvar_to_sym_ex = Expr(:(=),depvar,symbols(string(depvar)))
-    @eval $depvar_to_sym_ex
+      # Declare the SymEngine symbols
+      symtup,paramtup = symbolize(syms,param_dict.keys)
+      depvar_to_sym_ex = Expr(:(=),depvar,symbols(string(depvar)))
+      @eval $depvar_to_sym_ex
 
-    # Set Internal γ, used as a symbol for letting users pass an extra scalar
-    γ = symbols("internal_γ")
+      # Set Internal γ, used as a symbol for letting users pass an extra scalar
+      γ = symbols("internal_γ")
 
-    # Build the symbolic functions
+      # Build the symbolic functions
 
-    symfuncs = Vector{SymEngine.Basic}(numsyms)
+      symfuncs = Vector{SymEngine.Basic}(numsyms)
 
-    for i in eachindex(funcs)
-      funcex = funcs[i]
-      tmp = @eval $funcex
-      symfuncs[i] = SymEngine.Basic(tmp)
-    end
-
-    if opts[:build_tgrad]
-      try
-        symtgrad = Vector{SymEngine.Basic}(numsyms)
-        for i in eachindex(symfuncs)
-          symtgrad[i] = diff(SymEngine.Basic(symfuncs[i]),depvar)
-        end
-        tgrad_exists = true
-        tgradex = build_tgrad_func(symtgrad,indvar_dict,param_dict,inline_dict)
-      catch err
-        warn("Time Derivative Gradient could not invert")
+      for i in eachindex(funcs)
+        funcex = funcs[i]
+        tmp = @eval $funcex
+        symfuncs[i] = SymEngine.Basic(tmp)
       end
-    end
 
-    if opts[:build_jac]
-      try #Jacobians and Hessian
-        # Build the Jacobian Matrix of SymEngine Expressions
-        symjac = Matrix{SymEngine.Basic}(numsyms,numsyms)
-        for i in eachindex(funcs)
-          funcex = funcs[i]
-          symfunc = @eval $funcex
-          for j in eachindex(symtup)
-            symjac[i,j] = diff(SymEngine.Basic(symfuncs[i]),symtup[j])
+      if opts[:build_tgrad]
+        try
+          symtgrad = Vector{SymEngine.Basic}(numsyms)
+          for i in eachindex(symfuncs)
+            symtgrad[i] = diff(SymEngine.Basic(symfuncs[i]),depvar)
           end
+          tgrad_exists = true
+          tgradex = build_tgrad_func(symtgrad,indvar_dict,param_dict,inline_dict)
+        catch err
+          warn("Time Derivative Gradient could not invert")
         end
+      end
 
-        # Build the Julia function
-        Jex = build_jac_func(symjac,indvar_dict,param_dict,inline_dict)
-        jac_exists = true
-
-        if opts[:build_expjac]
-          try
-            expjac = expm(γ*symjac) # This does not work, which is why disabled
-            expJex = build_jac_func(expjac,indvar_dict,param_dict,inline_dict)
-            expjac_exists = true
-          catch
-            warn("Jacobian could not exponentiate")
-          end
-        end
-
-        if opts[:build_invjac]
-          try # Jacobian Inverse
-            invjac = inv(symjac)
-            invJex = build_jac_func(invjac,indvar_dict,param_dict,inline_dict)
-            invjac_exists = true
-          catch err
-            warn("Jacobian could not invert")
-          end
-        end
-        if opts[:build_invW]
-          try # Rosenbrock-W Inverse
-            syminvW = inv(M - γ*symjac)
-            syminvW_t = inv(M/γ - symjac)
-            invWex = build_jac_func(syminvW,indvar_dict,param_dict,inline_dict)
-            invW_exists = true
-            invWex_t = build_jac_func(syminvW_t,indvar_dict,param_dict,inline_dict)
-            invW_t_exists = true
-          catch err
-            warn("Rosenbrock-W could not invert")
-          end
-        end
-        if opts[:build_hes]
-          try # Hessian
-            symhes = Matrix{SymEngine.Basic}(numsyms,numsyms)
-            for i in eachindex(funcs), j in eachindex(symtup)
-              symhes[i,j] = diff(symjac[i,j],symtup[j])
+      if opts[:build_jac]
+        try #Jacobians and Hessian
+          # Build the Jacobian Matrix of SymEngine Expressions
+          symjac = Matrix{SymEngine.Basic}(numsyms,numsyms)
+          for i in eachindex(funcs)
+            funcex = funcs[i]
+            symfunc = @eval $funcex
+            for j in eachindex(symtup)
+              symjac[i,j] = diff(SymEngine.Basic(symfuncs[i]),symtup[j])
             end
-            # Build the Julia function
-            Hex = build_jac_func(symhes,indvar_dict,param_dict,inline_dict)
-            hes_exists = true
-            if opts[:build_invhes]
-              try # Hessian Inverse
-                invhes = inv(symhes)
-                invHex = build_jac_func(invhes,indvar_dict,param_dict,inline_dict)
-                invhes_exists = true
-              catch err
-                warn("Hessian could not invert")
+          end
+
+          # Build the Julia function
+          Jex = build_jac_func(symjac,indvar_dict,param_dict,inline_dict)
+          jac_exists = true
+
+          if opts[:build_expjac]
+            try
+              expjac = expm(γ*symjac) # This does not work, which is why disabled
+              expJex = build_jac_func(expjac,indvar_dict,param_dict,inline_dict)
+              expjac_exists = true
+            catch
+              warn("Jacobian could not exponentiate")
+            end
+          end
+
+          if opts[:build_invjac]
+            try # Jacobian Inverse
+              invjac = inv(symjac)
+              invJex = build_jac_func(invjac,indvar_dict,param_dict,inline_dict)
+              invjac_exists = true
+            catch err
+              warn("Jacobian could not invert")
+            end
+          end
+          if opts[:build_invW]
+            try # Rosenbrock-W Inverse
+              syminvW = inv(M - γ*symjac)
+              syminvW_t = inv(M/γ - symjac)
+              invWex = build_jac_func(syminvW,indvar_dict,param_dict,inline_dict)
+              invW_exists = true
+              invWex_t = build_jac_func(syminvW_t,indvar_dict,param_dict,inline_dict)
+              invW_t_exists = true
+            catch err
+              warn("Rosenbrock-W could not invert")
+            end
+          end
+          if opts[:build_hes]
+            try # Hessian
+              symhes = Matrix{SymEngine.Basic}(numsyms,numsyms)
+              for i in eachindex(funcs), j in eachindex(symtup)
+                symhes[i,j] = diff(symjac[i,j],symtup[j])
+              end
+              # Build the Julia function
+              Hex = build_jac_func(symhes,indvar_dict,param_dict,inline_dict)
+              hes_exists = true
+              if opts[:build_invhes]
+                try # Hessian Inverse
+                  invhes = inv(symhes)
+                  invHex = build_jac_func(invhes,indvar_dict,param_dict,inline_dict)
+                  invhes_exists = true
+                catch err
+                  warn("Hessian could not invert")
+                end
               end
             end
           end
+        catch err
+          warn("Failed to build the Jacoboian. This means the Hessian is not built as well.")
         end
-      catch err
-        warn("Failed to build the Jacoboian. This means the Hessian is not built as well.")
-      end
-    end # End Jacobian tree
+      end # End Jacobian tree
 
-    if opts[:build_dpfuncs]
-      try # Parameter Gradients
-        d_paramfuncs  = Vector{Vector{Expr}}(numparams)
-        for i in eachindex(paramtup)
-          tmp_dpfunc = Vector{Expr}(length(funcs))
-          for j in eachindex(funcs)
-            funcex = funcs[j]
-            symfunc = @eval $funcex
-            d_curr = diff(SymEngine.Basic(symfunc),paramtup[i])
-            param_symjac[j,i] = d_curr
-            symfunc_str = parse(string(d_curr))
-            if typeof(symfunc_str) <: Number
-              tmp_dpfunc[j] = :(1*$symfunc_str)
-            elseif typeof(symfunc_str) <: Symbol
-              tmp_dpfunc[j] = :(1*$symfunc_str)
-            else
-              tmp_dpfunc[j] = symfunc_str
+      if opts[:build_dpfuncs]
+        try # Parameter Gradients
+          d_paramfuncs  = Vector{Vector{Expr}}(numparams)
+          for i in eachindex(paramtup)
+            tmp_dpfunc = Vector{Expr}(length(funcs))
+            for j in eachindex(funcs)
+              funcex = funcs[j]
+              symfunc = @eval $funcex
+              d_curr = diff(SymEngine.Basic(symfunc),paramtup[i])
+              param_symjac[j,i] = d_curr
+              symfunc_str = parse(string(d_curr))
+              if typeof(symfunc_str) <: Number
+                tmp_dpfunc[j] = :(1*$symfunc_str)
+              elseif typeof(symfunc_str) <: Symbol
+                tmp_dpfunc[j] = :(1*$symfunc_str)
+              else
+                tmp_dpfunc[j] = symfunc_str
+              end
             end
+            d_paramfuncs[i] = tmp_dpfunc
           end
-          d_paramfuncs[i] = tmp_dpfunc
-        end
-        d_pfuncs = build_p_funcs(d_paramfuncs,indvar_dict,param_dict,inline_dict)
-        pderiv_exists = true
+          d_pfuncs = build_p_funcs(d_paramfuncs,indvar_dict,param_dict,inline_dict)
+          pderiv_exists = true
 
-        # Now build the parameter Jacobian
-        param_symjac_ex = Matrix{Expr}(numsyms,numparams)
-        for i in 1:numparams
-          param_symjac_ex[:,i] = d_paramfuncs[i]
-        end
+          # Now build the parameter Jacobian
+          param_symjac_ex = Matrix{Expr}(numsyms,numparams)
+          for i in 1:numparams
+            param_symjac_ex[:,i] = d_paramfuncs[i]
+          end
 
-        param_Jex = build_jac_func(param_symjac_ex,indvar_dict,param_dict,inline_dict,params_from_function=false)
-        param_jac_exists = true
-      catch err
-        warn("Failed to build the parameter derivatives.")
+          param_Jex = build_jac_func(param_symjac_ex,indvar_dict,param_dict,inline_dict,params_from_function=false)
+          param_jac_exists = true
+        catch err
+          warn("Failed to build the parameter derivatives.")
+        end
       end
+    catch err
+      warn("Symbolic calculations could not initiate. Likely there's a function which is not differentiable by SymEngine.")
     end
-  catch err
-    warn("Symbolic calculations could not initiate. Likely there's a function which is not differentiable by SymEngine.")
   end
 
   # Build the type
