fde3076884f6894eafcd7b2b8f0aab94dbc893a5
ChrisRackauckas <me@chrisrackauckas.com>
2017-02-06 07:47:41 -0800

    change p to params

--- src/parameterized_function_type.jl
+++ src/parameterized_function_type.jl
@@ -1,6 +1,6 @@
 type ParameterizedFunction{isinplace,F,P} <: AbstractParameterizedFunction{isinplace}
   f::F
-  p::P
+  params::P
 end
 
 Base.@pure function ParameterizedFunction(f,p)
@@ -8,14 +8,14 @@ Base.@pure function ParameterizedFunction(f,p)
   ParameterizedFunction{isinplace,typeof(f),typeof(p)}(f,p)
 end
 
-(pf::ParameterizedFunction{true,F,P}){F,P}(t,u,du) = pf.f(t,u,pf.p,du)
+(pf::ParameterizedFunction{true,F,P}){F,P}(t,u,du) = pf.f(t,u,pf.params,du)
 (pf::ParameterizedFunction{true,F,P}){F,P}(t,u,params,du) = pf.f(t,u,params,du)
-(pf::ParameterizedFunction{false,F,P}){F,P}(t,u) = pf.f(t,u,pf.p)
+(pf::ParameterizedFunction{false,F,P}){F,P}(t,u) = pf.f(t,u,pf.params)
 (pf::ParameterizedFunction{false,F,P}){F,P}(t,u,params) = pf.f(t,u,params)
 
 type DAEParameterizedFunction{isinplace,F,P} <: AbstractParameterizedFunction{isinplace}
   f::F
-  p::P
+  params::P
 end
 
 Base.@pure function DAEParameterizedFunction(f,p)
@@ -23,14 +23,14 @@ Base.@pure function DAEParameterizedFunction(f,p)
   DAEParameterizedFunction{isinplace,typeof(f),typeof(p)}(f,p)
 end
 
-(pf::DAEParameterizedFunction{true,F,P}){F,P}(t,u,du,out) = pf.f(t,u,pf.p,du,out)
+(pf::DAEParameterizedFunction{true,F,P}){F,P}(t,u,du,out) = pf.f(t,u,pf.params,du,out)
 (pf::DAEParameterizedFunction{true,F,P}){F,P}(t,u,params,du,out) = pf.f(t,u,params,du,out)
-(pf::DAEParameterizedFunction{false,F,P}){F,P}(t,u,du) = pf.f(t,u,pf.p,du)
+(pf::DAEParameterizedFunction{false,F,P}){F,P}(t,u,du) = pf.f(t,u,pf.params,du)
 (pf::DAEParameterizedFunction{false,F,P}){F,P}(t,u,params,du) = pf.f(t,u,params,du)
 
 type DDEParameterizedFunction{isinplace,F,P} <: AbstractParameterizedFunction{isinplace}
   f::F
-  p::P
+  params::P
 end
 
 Base.@pure function DDEParameterizedFunction(f,p)
@@ -38,7 +38,7 @@ Base.@pure function DDEParameterizedFunction(f,p)
   DDEParameterizedFunction{isinplace,typeof(f),typeof(p)}(f,p)
 end
 
-(pf::DDEParameterizedFunction{true,F,P}){F,P}(t,u,h,du) = pf.f(t,u,h,pf.p,du)
+(pf::DDEParameterizedFunction{true,F,P}){F,P}(t,u,h,du) = pf.f(t,u,h,pf.params,du)
 (pf::DDEParameterizedFunction{true,F,P}){F,P}(t,u,h,params,du) = pf.f(t,u,h,params,du)
-(pf::DDEParameterizedFunction{false,F,P}){F,P}(t,u,h) = pf.f(t,u,h,pf.p)
+(pf::DDEParameterizedFunction{false,F,P}){F,P}(t,u,h) = pf.f(t,u,h,pf.params)
 (pf::DDEParameterizedFunction{false,F,P}){F,P}(t,u,h,params) = pf.f(t,u,h,params)
