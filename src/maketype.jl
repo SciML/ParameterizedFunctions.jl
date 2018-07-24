5322c008bba617b10d05ffda4f4ff43b49d5bfe2
Xingjian Guo <xg703@nyu.edu>
2018-07-24 17:33:32 -0400

    Mass matrix hotfix

--- src/maketype.jl
+++ src/maketype.jl
@@ -18,10 +18,12 @@ function maketype(name,param_dict,origex,funcs,syms,fex;
                   invjac_expr=:(),
                   invW_expr=:(),
                   invW_t_expr=:(),
-                  param_jac_expr=:())
+                  param_jac_expr=:(),
+                  mm_expr=:())
 
-    typeex = :(mutable struct $name{F,J,T,W,Wt,PJ,TT1,TT2} <: DiffEqBase.AbstractParameterizedFunction{true}
+    typeex = :(mutable struct $name{F,MM,J,T,W,Wt,PJ,TT1,TT2} <: DiffEqBase.AbstractParameterizedFunction{true}
         f::F
+        mass_matrix::MM
         analytic::Nothing
         jac::J
         tgrad::T
@@ -68,7 +70,7 @@ function maketype(name,param_dict,origex,funcs,syms,fex;
     param_Jex_ex = Meta.quot(param_Jex)
 
     constructorex = :($(name)() =
-                  $(name)($f_expr,nothing,
+                  $(name)($f_expr,$mm_expr,nothing,
                   $jac_expr,$tgrad_expr,$invW_expr,$invW_t_expr,$param_jac_expr,
                   $new_ex,$funcs,$pfuncs,$d_pfuncs,$syms,$symjac,$symtgrad,
                   $tgradex_ex,$Jex_ex,$expJex_ex,$param_Jex_ex,
