85086e3b3c2edcb6ec23e767eaa6df49d396b6bb
ArnoStrouwen <arno.strouwen@telenet.be>
2024-04-02 22:01:10 +0200

    reapply formatter

--- test/qa.jl
+++ test/qa.jl
@@ -8,5 +8,5 @@ using ParameterizedFunctions, Aqua
     Aqua.test_project_extras(ParameterizedFunctions)
     Aqua.test_stale_deps(ParameterizedFunctions)
     Aqua.test_unbound_args(ParameterizedFunctions)
-    Aqua.test_undefined_exports(ParameterizedFunctions, broken=true)
+    Aqua.test_undefined_exports(ParameterizedFunctions, broken = true)
 end
